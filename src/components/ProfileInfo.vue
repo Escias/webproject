fdb48dee59ca28dbf892e12137b2883e7d73a3c3
Escias <ljimmy98@hotmail.fr>
2020-12-03 17:08:45 +0100

    some content for profile info

---
+++ src/components/ProfileInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card
+      elevation="3"
+      shaped
+  >
+    <v-avatar>
+      <img src="@/assets/alien.png" alt="">
+    </v-avatar>
+    <h3>Username</h3>
+    <p>Firstname Lastname</p>
+    <p><v-icon>mdi-map-marker</v-icon> City</p>
+    <p>Quote - I'm stupid</p>
+    <p>2000-01-01</p>
+    <v-btn
+        elevation="1"
+        tile
+        small
+    >
+      Edit Profile
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ProfileInfo"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+ <ProfileInfo/>
+</template>
+
+<script>
+import ProfileInfo from "@/components/ProfileInfo";
+export default {
+name: "Profile",
+  components: {
+   ProfileInfo,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
